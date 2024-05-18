97c562b6c25b7c2cb0aa569872e87c488f72692a
Shadkoech <shadrackipruto@gmail.com>
2024-05-18 15:55:51 +0300

    Setup and establishment of backend login implementation

---
+++ olkisir_backend/account/models.py
@@ -0,0 +1,51 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # These fields tie to the roles!
+    ADMIN = 1
+    DISPATCH_CHEMIST = 2
+    DRIVER = 3
+    TRADER = 4
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (DISPATCH_CHEMIST, 'Dispatch_chemist'),
+        (DRIVER, 'Driver'),
+        (TRADER, 'Trader')
+    )
+    
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    # Roles created here
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    username = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=3)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
