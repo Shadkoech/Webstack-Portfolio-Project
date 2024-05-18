4dc7070dadc0716f3443079842b2bbf3f0624b94
Abokiey <sweneysam@gmail.com>
2024-05-18 20:38:53 +0300

    creating of login and register components

--- olkisir-frontend/src/App.jsx
+++ olkisir-frontend/src/App.jsx
@@ -1,15 +1,36 @@
+import React, { useState } from "react";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+import Dashboard from "./components/shared/dashboard";
+import backgroundImage from "/oil.jpg";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-    
-  )
+    <div className="flex flex-col items-center justify-center h-screen"
+    style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
+
+      {isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        showLogin ? <Login toggleForm={toggleForm} handleLogin={handleLogin} /> : <Register toggleForm={toggleForm} handleLogin={handleLogin} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
