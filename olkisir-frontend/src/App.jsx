70ea9aafea378680b55d306b4d74da5ad548fa93
Shadkoech <shadrackipruto@gmail.com>
2024-05-19 14:57:47 +0300

    Front-end update
    Added a suitable background photo

--- olkisir-frontend/src/App.jsx
+++ olkisir-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import Dashboard from "./components/shared/dashboard";
-import backgroundImage from "/oil.jpg";
+import backgroundImage from "/oilbg6.jpg"; // Adjust the path to your image
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
@@ -17,13 +17,17 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen"
-    style={{
+    <div
+      className="flex flex-col items-center justify-center h-screen"
+      style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}>
-
+        backgroundRepeat: "no-repeat",
+        // Optional: Ensures background stays fixed while content scrolls
+        backgroundAttachment: "fixed",
+      }}
+    >
       {isLoggedIn ? (
         <Dashboard />
       ) : (
