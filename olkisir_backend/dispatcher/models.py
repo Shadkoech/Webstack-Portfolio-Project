f2311fbd3f9be5ae16011e04264da6b53bd5fc19
Norberts <mainanorbert@gmail.comm>
2024-05-29 21:42:02 +0300

    implemented return form backend and frontend

--- olkisir_backend/dispatcher/models.py
+++ olkisir_backend/dispatcher/models.py
@@ -47,7 +47,7 @@ class Reason(models.Model):
 
 
 class Return(models.Model):
-    order_id = models.ForeignKey(Order, null=True, unique=True, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Order, null=True, on_delete=models.CASCADE)
     products =  JSONField(default=list, blank=True)
     # product_returned = models.ForeignKey(Product, on_delete=models.CASCADE)
     # return_reason = models.ForeignKey(Reason, on_delete=models.CASCADE)
