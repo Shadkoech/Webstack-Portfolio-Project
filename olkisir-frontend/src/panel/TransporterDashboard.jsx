363dbf80b4e5945e81f54c3322a49cb7d9dbea76
Norberts <mainanorbert@gmail.com>
2024-05-31 20:51:53 +0300

    implemented frontend functionality where the clicked component is highlighted

--- olkisir-frontend/src/panel/TransporterDashboard.jsx
+++ olkisir-frontend/src/panel/TransporterDashboard.jsx
@@ -88,7 +88,7 @@ export const TransporterDashboard = () => {
             <li>
               <a
                 onClick={() => setActiveComponent("home")}
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className={`flex items-center p-2 text-gray-900 ${activeComponent === 'home'? 'bg-gray-100': ''} rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group`}
               >
                 <svg
                   className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -105,7 +105,7 @@ export const TransporterDashboard = () => {
             <li>
               <a
                 onClick={() => setActiveComponent("orderlist")}
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                className={`flex items-center p-2 text-gray-900 rounded-lg ${activeComponent === 'orderlist'? 'bg-gray-100': ''} dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group`}
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
