dbd3c2c3f6cdb4b6de0d4757fbe9fe6ab7544ce3
mainanorbert <mainanorbert@gmail.com>
2024-06-02 17:37:48 +0300

    added documentation on backend django, added documentation on views and serializers

--- olkisir_backend/account/serializers.py
+++ olkisir_backend/account/serializers.py
@@ -8,11 +8,24 @@ from django.contrib.auth import authenticate
 
 
 class UserSerializers(serializers.ModelSerializer):
+    """
+    Serializer for User model. Handles the creation and validation of User objects.
+    """
+    
     class Meta:
-        model=User
-        fields=["email","first_name", "last_name","password" ,"username", "role"]
+        model = User
+        fields = ["email", "first_name", "last_name", "password", "username", "role"]
 
     def create(self, validated_data):
+        """
+        Create and return a new User instance, given the validated data.
+        
+        Args:
+            validated_data (dict): Validated data containing user information.
+        
+        Returns:
+            User: The newly created User instance.
+        """
         user = User(
             email=validated_data['email'],
             username=validated_data['username'],
@@ -20,17 +33,33 @@ class UserSerializers(serializers.ModelSerializer):
             last_name=validated_data['last_name'],
             role=validated_data['role']
         )
+        # Set the user's password using Django's built-in set_password method
         user.set_password(validated_data['password'])
+        # Save the user instance to the database
         user.save()
         return user
 
-
 class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login. Validates user credentials and returns user information if valid.
+    """
     email = serializers.EmailField()
     password = serializers.CharField(max_length=128, write_only=True)
     role = serializers.CharField(read_only=True)
 
     def validate(self, data):
+        """
+        Validate user credentials. If valid, return user information.
+        
+        Args:
+            data (dict): Data containing email and password.
+        
+        Returns:
+            dict: Validated user data containing email, password, and role.
+        
+        Raises:
+            serializers.ValidationError: If the login credentials are invalid.
+        """
         email = data['email']
         password = data['password']
         user = authenticate(email=email, password=password)
@@ -39,16 +68,17 @@ class UserLoginSerializer(serializers.Serializer):
             raise serializers.ValidationError("Invalid login credentials")
 
         try:
-         
             validation = {
                 'email': user.email,
                 "password": user.password,
                 'role': user.role,
             }
-
             return validation
         except User.DoesNotExist:
             raise serializers.ValidationError("Invalid login credentials")
-        
+
 class LogoutSerializer(serializers.Serializer):
+    """
+    Serializer for user logout. Validates the refresh token required for logging out.
+    """
     refresh_token = serializers.CharField(required=True)
--- olkisir_backend/dispatcher/views.py
+++ olkisir_backend/dispatcher/views.py
@@ -3,21 +3,38 @@ from rest_framework import permissions, viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 from .serializers import DispatchChemistSerializer, FetchOrderSerializer, TransporterSerializer, TraderSerializer, ProductSerializer, OrderSerializer, ReasonSerializer, ReturnSerializer
 
 
 class DispatchChemistViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for DispatchChemist.
+    """
     queryset = DispatchChemist.objects.all()
     serializer_class = DispatchChemistSerializer
 
+    # Uncomment the following line to enforce authentication
     # permission_classes = [permissions.IsAuthenticated]
 
 class TransporterViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Transporter.
+    """
     queryset = Transporter.objects.all()
     serializer_class = TransporterSerializer
     
     @action(detail=False, methods=['get'], url_path='by-username')
     def get_transporter_id_by_username(self, request):
+        """
+        Custom action to get a Transporter's ID by their username.
+        
+        Args:
+            request (Request): The request object containing query parameters.
+        
+        Returns:
+            Response: A Response object containing the transporter's ID or an error message.
+        """
         username = request.query_params.get('username')
         if not username:
             return Response({"error": "Username query parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -29,12 +46,23 @@ class TransporterViewSet(viewsets.ModelViewSet):
             return Response({"error": "Transporter not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class TraderViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Trader.
+    """
     queryset = Trader.objects.all()
     serializer_class = TraderSerializer
     
-    
     @action(detail=False, methods=['get'], url_path='by-username')
     def get_trader_id_by_username(self, request):
+        """
+        Custom action to get a Trader's ID by their username.
+        
+        Args:
+            request (Request): The request object containing query parameters.
+        
+        Returns:
+            Response: A Response object containing the trader's ID or an error message.
+        """
         username = request.query_params.get('username')
         if not username:
             return Response({"error": "Username query parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -46,16 +74,26 @@ class TraderViewSet(viewsets.ModelViewSet):
             return Response({"error": "Trader not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ReasonViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Reason.
+    """
     queryset = Reason.objects.all()
     serializer_class = ReasonSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Order.
+    """
     queryset = Order.objects.all()
-    # serializer_class = OrderSerializer
+    
+    # Dynamically select the serializer class based on the action
     def get_serializer_class(self):
         if self.action == 'list' or self.action == 'retrieve':
             return FetchOrderSerializer
@@ -63,6 +101,16 @@ class OrderViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['get'])
     def trader_orders(self, request, pk=None):
+        """
+        Custom action to get orders for a specific trader.
+        
+        Args:
+            request (Request): The request object.
+            pk (int): The primary key of the trader.
+        
+        Returns:
+            Response: A Response object containing the trader's orders or an error message.
+        """
         try:
             trader = Trader.objects.get(pk=pk)
         except Trader.DoesNotExist:
@@ -74,6 +122,16 @@ class OrderViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['get'])
     def transporter_orders(self, request, pk=None):
+        """
+        Custom action to get orders for a specific transporter.
+        
+        Args:
+            request (Request): The request object.
+            pk (int): The primary key of the transporter.
+        
+        Returns:
+            Response: A Response object containing the transporter's orders or an error message.
+        """
         try:
             transporter = Transporter.objects.get(pk=pk)
         except Transporter.DoesNotExist:
@@ -84,16 +142,28 @@ class OrderViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 class ReturnViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling CRUD operations for Return.
+    """
     queryset = Return.objects.all()
     serializer_class = ReturnSerializer
 
-from django.shortcuts import get_object_or_404
 class CustomOrder(APIView):
-    """Fetching order details"""
-
+    """
+    API view to fetch custom order details including associated chemist, transporter, and trader.
+    """
     
     def get(self, request, order_id):
-        """Function for returning order details"""
+        """
+        Handle GET request to return order details.
+        
+        Args:
+            request (Request): The request object.
+            order_id (int): The ID of the order.
+        
+        Returns:
+            Response: A Response object containing the order details and associated data.
+        """
         order = get_object_or_404(Order, id=order_id)
         
         chemist = order.dispatch_chemist
