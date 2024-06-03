26b8bd7e9bdcd6839f25e14f867775743f7c52b4
Shadkoech <shadrackipruto@gmail.com>
2024-06-03 18:06:16 +0300

    Deployment
    Correcting env setting

--- olkisir_backend/olkisir_backend/settings.py
+++ olkisir_backend/olkisir_backend/settings.py
@@ -25,10 +25,10 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env['DEBUG']
+DEBUG = env('DEBUG')
 
 
 
@@ -105,7 +105,7 @@ APPEND_SLASH = False
 
 
 DATABASES = {
-    'default': dj_database_url.parse(env['DB_URL'])
+    'default': dj_database_url.parse(env('DB_URL'))
 }
 
 
